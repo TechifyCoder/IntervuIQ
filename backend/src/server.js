69ba30cea51124491b8dd8e79a8f7686378635f3
Techifycoder <satishpateldada80@gmail.com>
2025-11-20 16:51:25 +0530

    fix: correct production environment check and static file serving in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -33,12 +33,15 @@ app.listen(ENV.PORT,() => {
     mongoConnect();
 })
 
-if(ENV.NODE_ENV = "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
 
-    app.get("/{*any}",(req,res) => {
-        res.sendFile((path.join(__dirname,"../frontend","dist","index.html")))
-    })
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
 }
 
 const startServer = async () => {
