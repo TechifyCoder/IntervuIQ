df14b5c0d1137703e104f8193814563143c92af0
Techifycoder <satishpateldada80@gmail.com>
2025-11-29 14:41:07 +0530

    Navbar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes,Route, Navigate } from 'react-router'
 import { Toaster } from "react-hot-toast";
 import { useUser } from '@clerk/clerk-react';
 import HomePage from './pages/HomePage';
+import Navbar from './component/Navbar';
+import ProblemPage from './pages/ProblemPage';
 
 function App() {
   const { isSignedIn, isLoaded } = useUser();
@@ -12,6 +14,7 @@ function App() {
     <>
     <Routes>
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}  />
+        <Route path="/problem" element={!isSignedIn ? <ProblemPage /> : <Navigate to={"/dashboard"} />}  />
     </Routes>
      <Toaster toastOptions={{ duration: 3000 }} />
     </>
--- frontend/src/pages/ProblemPage.jsx
+++ frontend/src/pages/ProblemPage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../component/Navbar';
 
 const ProblemPage = () => {
     return (
         <div>
-            
+            <Navbar />
         </div>
     );
 }
