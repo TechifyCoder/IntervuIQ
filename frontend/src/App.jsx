b9298aa47cf5fb30f9aac280eb950c963af28268
Techifycoder <satishpateldada80@gmail.com>
2025-11-30 18:15:11 +0530

    Add ProblemDetailPage and enhance ProblemPage with problem statistics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { useUser } from '@clerk/clerk-react';
 import HomePage from './pages/HomePage';
 import Navbar from './component/Navbar';
 import ProblemPage from './pages/ProblemPage';
+import ProblemDetailPage from './pages/ProblemDetailPage';
 
 function App() {
   const { isSignedIn, isLoaded } = useUser();
@@ -15,6 +16,7 @@ function App() {
     <Routes>
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />}  />
         <Route path="/problem" element={!isSignedIn ? <ProblemPage /> : <Navigate to={"/dashboard"} />}  />
+        <Route path="/problem/:id" element={!isSignedIn ? <ProblemDetailPage /> : <Navigate to={"/dashboard"} />}  />
     </Routes>
      <Toaster toastOptions={{ duration: 3000 }} />
     </>
--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
     const isActive = (path) => location.pathname === path;
     return (
-        <nav className='bg-base-100/80 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-200'>
+        <nav className='bg-base-100/50 backdrop-blur-md border-b border-primary/20 sticky top-0 z-50 shadow-200'>
             <div className='max-w-7xl mx-auto p-4 flex items-center justify-between'>
                 {/* LOGO */}
                 <Link
