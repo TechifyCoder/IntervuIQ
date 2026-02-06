636c2658d3ff67ddfe9d91c483ce72a11e2ed817
Techifycoder <166680237+TechifyCoder@users.noreply.github.com.>
2026-02-06 19:41:14 +0530

    feat: implement real-time coding session with video conferencing, integrated code editor, and backend protection.

--- backend/src/controller/chatController.js
+++ backend/src/controller/chatController.js
@@ -1,16 +1,16 @@
 import { chatClient } from "../lib/stream.js";
 
-export const getStraemzToken = async (req,res) => {
+export const getStraemzToken = async (req, res) => {
     try {
         const token = chatClient.createToken(req.user.clerkId)
         res.status(200).json({
             token,
             userId: req.user.clerkId,
             userName: req.user.name,
-            userImage: req.user.image,
+            userImage: req.user.profileImage,
         })
     } catch (error) {
         console.error("Internal getStream error")
-        res.status(500).json({msg:"Internal server error"})
+        res.status(500).json({ msg: "Internal server error" })
     }
 }
